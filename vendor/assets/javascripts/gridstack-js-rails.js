f38fceb1d8ea99e740ad8104ffcdce801d4f7a88
Andrew Foster <andy@sol1.com.au>
2017-04-02 00:38:56 +1100

    jQueryUI droppable now appears to be a dependency

--- vendor/assets/javascripts/gridstack-js-rails.js
+++ vendor/assets/javascripts/gridstack-js-rails.js
@@ -3,6 +3,7 @@
 //= require jquery-ui/widget
 //= require jquery-ui/widgets/mouse
 //= require jquery-ui/widgets/draggable
+//= require jquery-ui/widgets/droppable
 //= require jquery-ui/widgets/resizable
 //= require gridstack/gridstack
 //= require gridstack/gridstack.jQueryUI
